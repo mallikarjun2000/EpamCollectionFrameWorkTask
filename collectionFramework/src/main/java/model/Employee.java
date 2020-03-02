62a98c52cf564993e44eae50fe65508d557abae7
mallikarjun2000 <arjunmallik093@gmail.com>
2020-03-03 01:06:16 +0530

    Project completed

--- collectionFramework/src/main/java/model/Employee.java
+++ collectionFramework/src/main/java/model/Employee.java
@@ -13,51 +13,35 @@ public class Employee {
 		this.address = address;
 		this.salary = salary;
 	}
-	/**
-	 * @return the name
-	 */
+	
 	public String getName() {
 		return name;
 	}
-	/**
-	 * @param name the name to set
-	 */
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-	/**
-	 * @return the age
-	 */
+	
 	public Integer getAge() {
 		return age;
 	}
-	/**
-	 * @param age the age to set
-	 */
+	
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-	/**
-	 * @return the address
-	 */
+	
 	public String getAddress() {
 		return address;
 	}
-	/**
-	 * @param address the address to set
-	 */
+	
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	/**
-	 * @return the salary
-	 */
+	
 	public Double getSalary() {
 		return salary;
 	}
-	/**
-	 * @param salary the salary to set
-	 */
+	
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
